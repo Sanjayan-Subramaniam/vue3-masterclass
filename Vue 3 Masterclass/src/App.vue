a2869de5783df59a72a7a6b10897fe4245e989a9
Oruculi <hello@oruculi.com>
2022-05-16 16:03:33 +1000

    Added HomePage to Vue Router

--- Vue 3 Masterclass/src/App.vue
+++ Vue 3 Masterclass/src/App.vue
@@ -5,31 +5,21 @@ import PageHome from '@/components/PageHome.vue';
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <img
+    alt="Vue logo"
+    class="logo"
+    src="@/assets/logo.svg"
+    width="125"
+    height="125"
+  />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <PageHome />
-  <RouterView />
+  <div class="container">
+    <PageHome />
+    <RouterView />
+  </div>
 </template>
 
 <style>
 @import '@/assets/base.css';
-@import '@/assets/style.css'
-
-
+@import '@/assets/style.css';
 </style>
--- Vue 3 Masterclass/src/components/PageHome.vue
+++ Vue 3 Masterclass/src/components/PageHome.vue
@@ -50,9 +50,7 @@ const userById = (userId) => {
 </template>
 
 <style scoped>
-
-
-  .post-list {
+.post-list {
   margin-top: 20px;
 }
 
@@ -69,7 +67,7 @@ const userById = (userId) => {
 
 @media (max-width: 820px) {
   .post {
-      padding: 0;
+    padding: 0;
   }
 }
 
@@ -89,29 +87,29 @@ const userById = (userId) => {
 
 @media (max-width: 820px) {
   .post .user-info {
-      order: -2;
-      flex-direction: row;
-      justify-content: flex-start;
-      background: rgba(73, 89, 96, 0.06);
-      margin-right: 0;
-      padding: 5px;
-      padding-left: 10px;
+    order: -2;
+    flex-direction: row;
+    justify-content: flex-start;
+    background: rgba(73, 89, 96, 0.06);
+    margin-right: 0;
+    padding: 5px;
+    padding-left: 10px;
   }
 
   .post .user-info .avatar-large {
-      height: 35px;
-      width: 35px;
-      margin-right: 5px;
-      order: 1;
+    height: 35px;
+    width: 35px;
+    margin-right: 5px;
+    order: 1;
   }
 
   .post .user-info .user-name {
-      order: 2;
+    order: 2;
   }
 
   .post .user-info > * {
-      margin-right: 5px;
-      margin-bottom: 0;
+    margin-right: 5px;
+    margin-bottom: 0;
   }
 }
 
@@ -125,18 +123,18 @@ const userById = (userId) => {
 
 @media (max-width: 820px) {
   .post .post-date {
-      order: -1;
-      flex-basis: 40%;
-      background: rgba(73, 89, 96, 0.06);
-      padding-right: 10px;
-      padding-top: 16px;
-      margin-bottom: 0px;
+    order: -1;
+    flex-basis: 40%;
+    background: rgba(73, 89, 96, 0.06);
+    padding-right: 10px;
+    padding-top: 16px;
+    margin-bottom: 0px;
   }
 }
 
 @media (max-width: 720px) {
   .post {
-      padding: 0px;
+    padding: 0px;
   }
 }
 
@@ -151,7 +149,9 @@ const userById = (userId) => {
   word-break: break-word;
 }
 
-.post-content h1, .post-content h2, .post-content h3 {
+.post-content h1,
+.post-content h2,
+.post-content h3 {
   margin-bottom: 0;
 }
 
@@ -182,15 +182,15 @@ const userById = (userId) => {
   left: -25px;
   font-size: 42px;
   font-family: FontAwesome;
-  content: "\f10e";
+  content: '\f10e';
   color: #263959;
 }
 
 @media (max-width: 820px) {
   .post-content blockquote.big::before {
-      top: -15px;
-      left: -18px;
-      font-size: 32px;
+    top: -15px;
+    left: -18px;
+    font-size: 32px;
   }
 }
 
@@ -233,15 +233,15 @@ const userById = (userId) => {
   left: -20px;
   font-size: 42px;
   font-family: FontAwesome;
-  content: "\f10e";
+  content: '\f10e';
   color: #263959;
 }
 
 @media (max-width: 820px) {
   .post-content blockquote.small::before {
-      top: -18px;
-      left: -15px;
-      font-size: 32px;
+    top: -18px;
+    left: -15px;
+    font-size: 32px;
   }
 }
 
@@ -283,7 +283,7 @@ const userById = (userId) => {
   font-weight: 100;
   font-style: italic;
   font-size: 17px;
-  letter-spacing: .15px;
+  letter-spacing: 0.15px;
 }
 
 .post-content blockquote.simple::before {
@@ -292,15 +292,15 @@ const userById = (userId) => {
   left: -25px;
   font-size: 42px;
   font-family: FontAwesome;
-  content: "\f10e";
+  content: '\f10e';
   color: #263959;
 }
 
 @media (max-width: 820px) {
   .post-content blockquote.simple::before {
-      top: -15px;
-      left: -18px;
-      font-size: 32px;
+    top: -15px;
+    left: -18px;
+    font-size: 32px;
   }
 }
 
@@ -317,7 +317,4 @@ const userById = (userId) => {
 .post-listing-editor {
   flex: 1 1 83%;
 }
-
-
-
 </style>
